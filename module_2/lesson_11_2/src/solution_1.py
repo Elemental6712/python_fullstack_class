2030ec884327f1573e75eb540fc9b8867f584029
Elemental6712 <150152272+Elemental6712@users.noreply.github.com>
2024-06-04 16:09:34 +0300

    Delete module_2/lesson_11_2/src directory

--- module_2/lesson_11_2/src/solution_1.py
+++
@@ -1,9 +0,0 @@
-list_prices_1 = [100, 200, 300]
-
-def sum_sales_with_for(list_prices):
-    summa = 0
-    for i in list_prices:
-        summa += i
-    print(*list_prices, sep=" + ", end=f' = {summa}')
-
-sum_sales_with_for(list_prices_1)
